17b4d4cf4aedacd3976dfa5034030cfe963cb7be
claudia <claudia.vitolo@gmail.com>
2021-02-14 17:16:24 +0000

    Fixed tests

--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -64,71 +64,22 @@ test_that("Try and retrieve hourly data", {
 
   x <- ukair_get_hourly_data(site_id = "ABD", years = "2014")
 
-  expect_that(dim(x)[1] >= 8760, equals(TRUE))
+  expect_true(dim(x)[1] >= 8760)
 
-  expect_that(x[1,1], equals(structure(list(
-    datetime = structure(1388538000,
-                         class = c("POSIXct", "POSIXt"),
-                         tzone = "UTC")),
-    .Names = "datetime",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,2], equals(structure(list(
-    SiteID = structure(1L,
-                       .Label = "ABD",
-                       class = "factor")),
-    .Names = "SiteID",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,3], equals(structure(list(
-    PM.sub.10..sub..particulate.matter..Hourly.measured. = 16.1),
-    .Names = "PM.sub.10..sub..particulate.matter..Hourly.measured.",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,4], equals(structure(list(
-    Nitric.oxide = 1.6402),
-    .Names = "Nitric.oxide",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,5], equals(structure(list(
-    Nitrogen.dioxide = 11.28311),
-    .Names = "Nitrogen.dioxide",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,6], equals(structure(list(
-    Nitrogen.oxides.as.nitrogen.dioxide = 13.79805),
-    .Names = "Nitrogen.oxides.as.nitrogen.dioxide",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,7], equals(structure(list(
-    Non.volatile.PM.sub.10..sub...Hourly.measured. = 15.1),
-    .Names = "Non.volatile.PM.sub.10..sub...Hourly.measured.",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,8], equals(structure(list(
-    Non.volatile.PM.sub.2.5..sub...Hourly.measured. = 7.3),
-    .Names = "Non.volatile.PM.sub.2.5..sub...Hourly.measured.",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,9], equals(structure(list(
-    Ozone = 54.94827), .Names = "Ozone",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,10], equals(structure(list(
-    PM.sub.2.5..sub..particulate.matter..Hourly.measured. = 9.2),
-    .Names = "PM.sub.2.5..sub..particulate.matter..Hourly.measured.",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,11], equals(structure(list(
-    Volatile.PM.sub.10..sub...Hourly.measured. = 1),
-    .Names = "Volatile.PM.sub.10..sub...Hourly.measured.",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
-  expect_that(x[1,12], equals(structure(list(
-    Volatile.PM.sub.2.5..sub...Hourly.measured. = 1.9),
-    .Names = "Volatile.PM.sub.2.5..sub...Hourly.measured.",
-    row.names = c(NA, -1L),
-    class = c("tbl_df", "tbl", "data.frame"))))
+  expect_equal(x$datetime[1],
+               structure(1388538000, class = c("POSIXct", "POSIXt"),
+                         tzone = "UTC"))
+  expect_equal(x$SiteID[1], structure(1L, .Label = "ABD", class = "factor"))
+  expect_equal(x$PM.sub.10..sub..particulate.matter..Hourly.measured.[1], 16.1)
+  expect_equal(x$Nitric.oxide[1], 1.6402)
+  expect_equal(x$Nitrogen.dioxide[1], 11.28311)
+  expect_equal(x$Nitrogen.oxides.as.nitrogen.dioxide[1], 13.79805)
+  expect_equal(x$Non.volatile.PM.sub.10..sub...Hourly.measured.[1], 15.1)
+  expect_equal(x$Non.volatile.PM.sub.2.5..sub...Hourly.measured.[1], 7.3)
+  expect_equal(x$Ozone[1], 54.94827)
+  expect_equal(x$PM.sub.2.5..sub..particulate.matter..Hourly.measured.[1], 9.2)
+  expect_equal(x$Volatile.PM.sub.10..sub...Hourly.measured.[1], 1)
+  expect_equal(x$Volatile.PM.sub.2.5..sub...Hourly.measured.[1], 1.9)
 
   closeAllConnections()
 
